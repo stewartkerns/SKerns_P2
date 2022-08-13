f4208b937470e060784eaa13c4cbf43b06613e30
Stewart <stewart.kerns@gmail.com>
2022-08-13 15:26:02 -0700

    Code works, need to finish descriptions of classes

--- src/SKerns_P2/MessageDecoder.java
+++ src/SKerns_P2/MessageDecoder.java
@@ -9,7 +9,7 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
- * This...................................................
+ * This TODO Put in a class description
  *
  * @author Stewart Kerns
  * @version 1.0
@@ -19,7 +19,7 @@ public class MessageDecoder {
      * This class is to create a Node for a linked list
      *
      * @author Stewart Kerns
-     * @verion 1.0
+     * @version 1.0
      */
     private class Node {
         //declare an integer value
@@ -27,6 +27,7 @@ public class MessageDecoder {
         char character;
         //declare a Node next
         Node next;
+
 
         /**
          * This constructor set the values of value and next for the node
@@ -74,60 +75,6 @@ public class MessageDecoder {
         last = null;
     }
 
-    /**
-     * This method returns an integer value for the size of the linked list
-     * @return int value representing the size of the linked list
-     */
-    public int size(){
-        //create a count
-        int count = 0;
-        //Create a Node object for traversing
-        Node p = first;
-
-        //traverse through the linked list
-        while (p != null){
-            //increment count for each node
-            count++;
-            p = p.next;
-        }
-
-        //return the count
-        return count;
-    }
-
-    /**
-     * This method removes any duplicate values from the linked list
-     */
-    public void removeDuplicate(){
-        //create nodes for the first and second values in the linked list
-        Node current = first.next;
-        Node pred = first;
-
-        //create a for loop to traverse through the linked list
-        for (int i = 1; i < size(); i++) {
-
-            //if the pred and current node are the same and current isn't null,
-            // delete current
-            while (current != null && current.value == pred.value) {
-                pred.next = current.next;
-
-                //for setting last if the list has been fully gone through
-                if (current.next == null) {
-                    last = pred;
-                }
-
-                //move current but stay in the while loop to check for multiple
-                //multiples
-                current = pred.next;
-            }
-
-            //if current isn't null, move to the next two nodes
-            if (current != null) {
-                pred = pred.next;
-                current = current.next;
-            }
-        }
-    }
 
     /**
      * This method checks to see if the linked list is empty and if so, it
@@ -135,7 +82,7 @@ public class MessageDecoder {
      *
      * @return boolean value for if the linked list is empty
      */
-    public boolean isEmpty() {
+    private boolean isEmpty() {
         return (first == null);
     }
 
@@ -145,7 +92,7 @@ public class MessageDecoder {
      *
      * @param number an integer input for the new node
      */
-    public void insertInOrder(int number, char character) {
+    private void insertInOrder(int number, char character) {
         //sets the current node to first
         Node current = first;
         //if the linked list is empty, it sets the first and last node to the
@@ -200,21 +147,6 @@ public class MessageDecoder {
         }
     }
 
-    /**
-     * Checks to see that the user-specified file name refers to a valid
-     * file on the disk and not a directory. Displays an error message to the
-     * user if that is not the case.
-     * @param fileName file name string to check
-     * @return true if file exists on disk and is not a directory
-     */
-    private static boolean isValidFile(String fileName) {
-        File path = new File(fileName);
-        boolean isValid = path.exists() && !path.isDirectory();
-        if (!isValid) {
-            // TODO: Display a proper error message
-        }
-        return isValid;
-    }
 
     /**
      * This method checks to make sure that the data being put in is valid
@@ -222,51 +154,131 @@ public class MessageDecoder {
      * @param number an integer for the data being input
      * @return boolean value of if the data is valid and good for use
      */
-    private boolean isGoodData(int number){
+    private boolean numOver0(int number){
+        //return if the number is larger than 0
         return number > 0;
     }
 
-    private void readFileIntoLinkedList(String fileName) throws IOException{
-        File file = new File(fileName);
+    /**
+     * This method reads a file into the linked list by reading one line at a
+     * time and then organizing the character by the number given by the file
+     * @param filename the filename for the file to be read and passed in
+     * @throws IOException
+     */
+    private void readFileIntoLinkedList(String filename) throws IOException{
+        //create a File and Scanner object for reading the file
+        File file = new File(filename);
         Scanner inputFile = new Scanner(file);
+
+        //declare a character and int that will be assigned to the node
         char character;
         int number;
+        //declare a final int for the minimum length of the line
+        final int STRING_MIN = 3;
+
+        //create a while loop to run until the end of the file
         while (inputFile.hasNext()){
+            //put the entire line into a string
             String line = inputFile.nextLine();
-            character = line.charAt(0);
-            number = Integer.parseInt(line.substring(2));
-//            number = line.charAt(2) - '0';
 
+            //if the string is longer than the min length, move into putting the
+            //data from the line into a node of the linked list
+            if (line.length() >= STRING_MIN) {
+                //take the first char of the string in for the character
+                character = line.charAt(0);
+                //separate the number area of the line and then pass it into
+                //parstInt in order to convert it to a number
+                number = Integer.parseInt(line.substring(2));
 
-
-            System.out.println(character);
-            System.out.println("This is number: " + number);
-//            System.out.println("This is a test of multiplication: " + (number * number));
-            if (isGoodData(number)){
-                insertInOrder(number, character);
+                //if the number is a valid number and not a repeat, insert it
+                //into the linked list
+                if (numOver0(number) && doesNotContainValue(number)) {
+                    insertInOrder(number, character);
+                }
             }
         }
+        //close the input file
+        inputFile.close();
     }
 
     /**
-     * This method prints out the contents of the linked list
+     * This method takes in a filename for a coded message and returns a string
+     * of the decoded message
+     * @param filename the filename of the coded message
+     * @return String of the message decoded
+     * @throws IOException
      */
-    public void printLinkedList() {
-        //set the current node to first
-        Node current = first;
+    public String getPlainTextMessage(String filename)throws IOException {
+        //pass the filename to readFileIntoLinkedList in order to create the
+        //linked list
+        readFileIntoLinkedList(filename);
 
-        //loop through the contents until the end and print each number
-        while (current != null) {
-            System.out.println(current.character);
-            //move current to the next value
-            current = current.next;
+        //create a node to traverse through the linked list
+        Node p = first;
+        //create a string that will be used to add each character into
+        String builtString = "";
+
+        //traverse the linked list and add each character in
+        while (p != null){
+            builtString += p.character;
+            p = p.next;
         }
+
+        //return the string after the linked list has been trasferred
+        return builtString;
     }
 
-    public void testPrint(String fileName) throws IOException{
-        readFileIntoLinkedList(fileName);
-        printLinkedList();
+    /**
+     * This method returns a boolean of whether it was able to find value of
+     * number in the linked list
+     * @param number an int that the method will be searching for
+     * @return boolean of if the value was found or not
+     */
+    private boolean doesNotContainValue(int number){
+        //create a node for traversing through the linked list
+        Node p = first;
 
+        //traverse through the linked list and search for the number
+        while (p != null){
+            //return false if the number is found
+            if (p.value == number){
+                return false;
+            }
+            p = p.next;
+        }
+
+        //return true if the number is not found in the linked list values
+        return true;
+    }
+
+    /**
+     * Checks to see that the user-specified file name refers to a valid
+     * file on the disk and not a directory. Displays an error message to the
+     * user if that is not the case.
+     * @param filename file name string to check
+     * @return true if file exists on disk and is not a directory
+     */
+    public static boolean isValidFile(String filename) {
+        //create a File object with the filename
+        File path = new File(filename);
+
+        //create a boolean for if the file exists and output a message if not
+        boolean isValid = path.exists() && !path.isDirectory();
+        if (!isValid) {
+            System.out.println("This is not a valid filename.");
+        }
+        //return the boolean value for if it is valid or not
+        return isValid;
+    }
+
+    /**
+     * This method clears the linked list of all values by setting first to null
+     */
+    public void clearLinkedList(){
+        //set first to null
+        first = null;
+        //set last to first
+        last = first;
     }
 
 }
--- src/SKerns_P2/SecretMessage.java
+++ src/SKerns_P2/SecretMessage.java
@@ -1,19 +1,115 @@
+/*
+ * Stewart Kerns
+ * CPSC 5002, Seattle University
+ * This is free and unencumbered software released into the public domain.
+ */
 package SKerns_P2;
 
-import javax.imageio.IIOException;
-import java.io.File;
-import java.io.IOException;
+//import the io wildcard
+import java.io.*;
+//import the Scanner class
+import java.util.Scanner;
 
+/**
+ * TODO write a description
+ */
 public class SecretMessage {
 
+    /**
+     * TODO write a description
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        final String FILENAME = "C:\\Users\\Stewart\\Desktop\\CPSC5002\\SKerns_P2\\src\\SKerns_P2\\rain.dat";
-        final String FILENAME2 = "C:\\Users\\Stewart\\Desktop\\CPSC5002\\SKerns_P2\\src\\SKerns_P2\\hello-1.dat";
-        final String FILENAME3 = "SKerns_P2/rain.dat";
-//        char space = 32;
-//        char a = 'a';
-//        System.out.println("Is this"+ space +"a space?");
+        //create a Scanner object for taking in user input
+        Scanner keyboardIn = new Scanner(System.in);
+        //create a new MessageDecoder object
         MessageDecoder newMessage = new MessageDecoder();
-        newMessage.testPrint(FILENAME);
+
+        //declare Strings for the decoded message and filename
+        String decodedMessage, filename;
+
+        //welcome the user to the program
+        welcome();
+
+        //use a do while loop to allow the user to run the program as many
+        //times as they want
+        do {
+            //Ask the user for the filename they would like to decode
+            filename = getUserFilename(keyboardIn);
+            //decode the message in the requested filename
+            decodedMessage = newMessage.getPlainTextMessage(filename);
+
+            //print out the decoded message
+            System.out.println("\nThe decoded message is written below:");
+            System.out.println(decodedMessage);
+
+            //clear the message out of newMessage before asking if the user
+            //would like to input another file
+            newMessage.clearLinkedList();
+
+            //ask the user if they want to repeat the program
+        } while (repeatProgram(keyboardIn));
+
+        //print a goodbye message to the user
+        goodbye();
+        //close the Scanner object
+        keyboardIn.close();
     }
+
+    /**
+     * This method asks the user to input the filename of the file they would
+     * like to use, it then checks that the file is valid and asks them again
+     * if not
+     * @param keyboardIn a Scanner object for taking in user input
+     * @return String value of the filename
+     */
+    public static String getUserFilename(Scanner keyboardIn){
+        //declare a String to hold the filename
+        String filename;
+
+        //create a do while loop to validate the users input
+        do {
+            //ask the user to input the filename
+            System.out.print("\nPlease input the filename you would like to " +
+                    "decode: ");
+            filename = keyboardIn.nextLine();
+            //validate that the filename is a valid path/file
+        } while (!MessageDecoder.isValidFile(filename));
+
+        //return the filename once it's valid
+        return filename;
+    }
+
+    /**
+     * This method asks the user if they want to repeat the program again and
+     * only returns false if the user enters "no"
+     * @param keyboardIn a Scanner object for taking in user input
+     * @return boolean value for if they answered no or not
+     */
+    public static boolean repeatProgram(Scanner keyboardIn){
+        //ask the user if they want to repeat the program
+        System.out.print("\nWould you like to decode another secret message?" +
+                "(enter no to quit): ");
+
+        //return true if they enter anything other than "no"
+        return !(keyboardIn.nextLine().equalsIgnoreCase("NO"));
+    }
+
+    /**
+     * This method prints a welcome message to the user
+     */
+    public static void welcome(){
+        System.out.println("This program reads an encoded message from a file" +
+                " supplied by the user and\n displays the contents of the " +
+                "message before it was encoded.");
+    }
+
+    /**
+     * This method prints a goodbye message to the user
+     */
+    public static void goodbye(){
+        System.out.println("Thank you for using the message decoder.");
+    }
+
 }
